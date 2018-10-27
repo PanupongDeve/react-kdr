93146d6bd875b9f75559990ac3348aebeca42997
PanupongDev <5635512110@psu.ac.th>
2018-10-27 12:29:04 +0700

    update config

--- material/src/class/ServicesAPI/Config.js
+++ material/src/class/ServicesAPI/Config.js
@@ -1,1 +1,3 @@
-export const ServerURL = "http://localhost:3003";
+export const ServerURL = `http://localhost:${PORT}`;
+
+const PORT = process.env.NODE_ENV === 'production' ? process.env.PORT : 3003;
