44e4343c6ab2ca38dcb239267cfdfaa75d174290
PanupongDev <5635512110@psu.ac.th>
2018-10-27 13:01:17 +0700

    fix bugs

--- material/src/class/ServicesAPI/Config.js
+++ material/src/class/ServicesAPI/Config.js
@@ -1,3 +1,5 @@
 const PORT = process.env.NODE_ENV === 'production' ? process.env.PORT : '3003';
+console.log(process.env.NODE_ENV );
+console.log(process.env.PORT);
 
 export const ServerURL = `http://localhost:${PORT}`;
