7c3a147acdd53f2216d1215a401032a90e901323
PanupongDeve <5635512110@psu.ac.th>
2019-01-08 15:54:18 +0700

    change default image

--- material/src/routes/app/routes/table/routes/product-table/EditModal/Fields.js
+++ material/src/routes/app/routes/table/routes/product-table/EditModal/Fields.js
@@ -78,7 +78,7 @@ class TextFields extends ComponentWithHandle {
       sizeSelected: "",
       modelLists: [],
       modelSelected: "",
-      imagePath: "assets/images-demo/image-icons/coming-soon.png",
+      imagePath: "assets/images/upload_file.png",
       price: '',
       priceA: '',
       priceB: '',
