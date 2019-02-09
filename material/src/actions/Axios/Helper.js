fa5fd48a8a0331befda948003273dc342f54779c
PanupongDeve <5635512110@psu.ac.th>
2019-02-09 20:50:40 +0700

    fixed error when JsonWebTokenError #60

--- material/src/actions/Axios/Helper.js
+++ material/src/actions/Axios/Helper.js
@@ -1,6 +1,16 @@
 import ErrorHandleMessage from '../../class/ErrorHandleMessage';
+import Storage from '../../class/Storage';
 
 const getMessageError = (error) => {
+    if (error
+        && error.response 
+        && error.response.data 
+        && error.response.data.result
+        && error.response.data.result.name === 'JsonWebTokenError') {
+            const storage = new Storage();
+            storage.removeStorage();
+            window.location.reload();
+    }
     const errorHandleMessage = new ErrorHandleMessage();
     errorHandleMessage.setErrorMessage(error);
     const errorMessage = errorHandleMessage.getErrorMessage();
